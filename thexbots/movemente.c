972148cbd521f483583a03c753118cca0a29643e
TheXbots <118382544+TheXbots@users.noreply.github.com>
2022-11-17 11:39:39 -0500

    movement

---
+++ thexbots/movemente.c
@@ -0,0 +1,36 @@
+void DoAction(char* dirorstraight, int length, int speed, int angle)
+{
+	if (dirorstraight	== "Straight") {
+		motor(motorB) = speed;
+		motor(motorC) = speed;
+	}
+	else if (dirorstraight == "Left") {
+		motor(motorB) = speed;
+		motor(motorC) = -speed;
+	}
+	else if (dirorstraight == "Right") {
+		motor(motorB) = -speed;
+		motor(motorC) = speed;
+		
+	}
+	else if (dirorstraight == "Back") {
+		motor(motorB) = -speed;
+		motor(motorC) = -speed;
+	
+	}
+	
+	if (dirorstraight == "Left") {
+		delay(7.06 * angle);
+	}
+	else if (dirorstraight == "Right") {
+		delay(7.06 * angle);
+	}
+	
+	else if (dirorstraight == "Straight") {
+		delay(length);
+	}
+	
+	else if (dirorstraight == "Back") {
+		delay(length);
+	}
+}
