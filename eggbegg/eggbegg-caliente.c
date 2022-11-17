d47429effd9f2a392beb891f6b01e7a6841365db
eggbegg <113047468+eggbegg@users.noreply.github.com>
2022-11-17 12:06:30 -0500

    fixed

--- eggbegg/eggbegg-caliente.c
+++ eggbegg/eggbegg-caliente.c
@@ -2,7 +2,8 @@
 void hardStop(int length) {
 	motor(motorB) = 0;
 	motor(motorC) = 0;
-	delay(length)
+	delay(length);
+}
 
 // smooth stop - to combat movement offset
 void smoothStop(int length, int initialspeed) {
@@ -12,22 +13,23 @@ void smoothStop(int length, int initialspeed) {
 	motor(motorB) = 0;
 	motor(motorC) = 0;
 	delay(length / 2);
+}
 
 void doAction(char* direction, int length, int speed, bool useAngle, bool useSmooth) {
 	// specify direction and execute
-	if (dirorstraight	== "Straight") {
+	if (direction	== "Straight") {
 		motor(motorB) = speed;
 		motor(motorC) = speed;
 	}
-	else if (dirorstraight == "Left") {
+	else if (direction == "Left") {
 		motor(motorB) = speed;
 		motor(motorC) = -speed;
 	}
-	else if (dirorstraight == "Right") {
+	else if (direction == "Right") {
 		motor(motorB) = -speed;
 		motor(motorC) = speed;
 	}
-	else if (dirorstraight == "Back") {
+	else if (direction == "Back") {
 		motor(motorB) = -speed;
 		motor(motorC) = -speed;
 	}
@@ -36,9 +38,9 @@ void doAction(char* direction, int length, int speed, bool useAngle, bool useSmo
 	if (useAngle == true) {
 		delay(7.1462476767676 * length);
 	}
-	else if (useangle == false) {
+	else if (useAngle == false) {
 		if (useSmooth == true) {
-			smoothStop(length);
+			smoothStop(length, speed);
 		}
 		else if (useSmooth == false) {
 			hardStop(length);
@@ -46,6 +48,7 @@ void doAction(char* direction, int length, int speed, bool useAngle, bool useSmo
 	}
 }
 
+// robot function
 task main() {
 	
 }
