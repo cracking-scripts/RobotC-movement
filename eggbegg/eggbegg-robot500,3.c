3f26934314af3ac78630217903bf1e2acfb8adfd
eggbegg <113047468+eggbegg@users.noreply.github.com>
2022-11-18 11:22:58 -0500

    Create eggbegg-robot500,3.c

---
+++ eggbegg/eggbegg-robot500,3.c
@@ -0,0 +1,30 @@
+// eggbeg robot 500 - 3
+// 2022-11-18
+
+#pragma config(Sensor, S4,     sonar,          sensorSONAR)
+#pragma config(Motor,  motorA,          mA,            tmotorNXT, PIDControl, encoder)
+#pragma config(Motor,  motorB,          mB,            tmotorNXT, PIDControl, encoder)
+#pragma config(Motor,  motorC,          mC,            tmotorNXT, PIDControl, encoder)
+
+void turn(char* direction) {
+	if (direction == "r") {
+		motor[motorB] = -50;
+		motor[motorC] = 50;
+		delay(636);
+	}
+	else if (direction == "l") {
+		motor[motorB] = 50;
+		motor[motorC] = -50;
+		delay(636);
+	}
+}
+
+task main() {
+	while (true) {
+		while (SensorValue[S4]>12) {
+		motor[motorB] = 100;
+		motor[motorC] = 100;
+		}
+		turn("r");
+	}
+}
