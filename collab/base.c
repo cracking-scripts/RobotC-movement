9bf55b5f1ad23893e2b3acecf18086931ee47a07
Sir Quindavious Bingchillingston IV <113047468+eggbegg@users.noreply.github.com>
2022-12-07 11:40:46 -0500

    Create base.c

---
+++ collab/base.c
@@ -0,0 +1,21 @@
+/*
+"SCRIPT NAME"
+RobotC Script - MONTH DD YYYY
+by "AUTHOR(S)"
+*/
+
+/*
+VARIABLES / FUNCTIONS USED
+VARIABLE (USE)
+FUNCTION (USE)
+*/
+
+// assign config
+#pragma config(Sensor, S3,     light,          sensorLightActive)
+#pragma config(Sensor, S4,     sonar,          sensorSONAR)
+
+// start task
+task main()
+{
+
+}
